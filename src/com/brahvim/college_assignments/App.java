8ce3f434e4490927dc2af647372ba28fb9f6e7cd
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-09 17:25:22 +0530

    I re-invented the wheel.
    In this case, the algorithm. Lemme know if you want something changed.

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -1,8 +1,25 @@
 package com.brahvim.college_assignments;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(final String[] p_args) {
+        System.out.print("Enter a number to sum the digits of: ");
+
+        final Scanner sc = new Scanner(System.in);
+        final long userInput = sc.nextLong();
+
+        long sum = 0;
+        long number = userInput;
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        System.out.println(String.format("Total: `%d`.", sum));
+        sc.close();
     }
 
 }
