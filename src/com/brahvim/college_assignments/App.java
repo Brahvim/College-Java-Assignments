3d227f810bd495c43f9b1ed43244dc59eb98d88c
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-09 17:00:39 +0530

    Haven't committed in forever!
    Was giving a friend IT support today :/

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -3,6 +3,12 @@ package com.brahvim.college_assignments;
 public class App {
 
     public static void main(final String[] p_args) {
+        System.out.println(String.format(
+                "Number of parameters: `%d`.", p_args.length));
+
+        for (int i = 0; i < p_args.length; i++)
+            System.out.println(String.format(
+                    "Argument number `%d`: `%s`.", i, p_args[i]));
     }
 
 }
