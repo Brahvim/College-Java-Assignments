1410809fbc33cb5f8520a0ea966ad84f91c4efef
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-09 17:32:39 +0530

    Wait...

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -2,7 +2,12 @@ package com.brahvim.college_assignments;
 
 public class App {
 
+    public static void isPrime(final long p_toCheck) {
+
+    }
+
     public static void main(final String[] p_args) {
+        
     }
 
 }
