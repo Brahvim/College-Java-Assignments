432b6e81a9b4ed590c81f93a867884a08fbec33c
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-09 17:31:34 +0530

    Piece of cake.

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -1,8 +1,20 @@
 package com.brahvim.college_assignments;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(final String[] p_args) {
+        System.out.print("Enter the number to display the multiplication table for: ");
+
+        final Scanner sc = new Scanner(System.in);
+        final long multiplier = sc.nextLong();
+
+        System.out.print("Till when?: ");
+        final long tableEnd = sc.nextLong() + 1;
+
+        for (int i = 1; i < tableEnd; i++)
+            System.out.println(String.format("`%d` * `%d` = `%d`.", multiplier, i, multiplier * i));
     }
 
 }
