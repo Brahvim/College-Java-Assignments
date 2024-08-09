e9faa114a8eb6fe4fa872bf048f2a46ab80be375
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-09 17:41:49 +0530

    I've been writing this for 4 years too!

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -1,13 +1,33 @@
 package com.brahvim.college_assignments;
 
+import java.util.Scanner;
+
 public class App {
 
-    public static void isPrime(final long p_toCheck) {
+    // I've been noting my implementations for this for years!
+    // This is the 6th one so far:
+    public static boolean isPrime(final long p_toCheck) {
+        if (p_toCheck < 2)
+            // If it's `1` or less, it ain't prime!
+            return false;
 
+        final long checkTill = (long) Math.sqrt(p_toCheck);
+
+        for (long i = 2; i <= checkTill; i++)
+            if (p_toCheck % i == 0)
+                return false;
+
+        return true;
     }
 
     public static void main(final String[] p_args) {
-        
+        System.out.print("Enter a number, I'll tell if it's a prime!: ");
+        final Scanner sc = new Scanner(System.in);
+        System.out.println(
+                App.isPrime(sc.nextLong())
+                        ? "Yes it is!"
+                        : "Nope! That's not a prime.");
+        sc.close();
     }
 
 }
