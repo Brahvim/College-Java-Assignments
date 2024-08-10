a55d0610567c5671e5aa4b3a5c40797e680b9bd4
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-10 18:08:47 +0530

    There is no longer a brain method.
    **And now I revert this commit.**

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -40,6 +40,18 @@ public class App {
 
     }
 
+    private static class AppStringIds {
+
+        int sourceStrId, destStrId;
+
+    }
+
+    private static class AppPermissions {
+
+        boolean overwriteDestination;
+
+    }
+
     public static void showHelp() {
         System.out.println(
                 """
@@ -68,66 +80,56 @@ public class App {
         System.exit(p_failure.exitCode);
     }
 
-    public static void main(final String[] p_args) {
-        if (p_args.length < 2) {
-            App.showHelp();
-            App.exitWithCode(AppExitCode.OK);
+    private static void performActualCopy(final String destPath, final String sourcePath, final File destFile,
+            final File sourceFile) {
+        try (final var is = new FileInputStream(sourceFile)) {
+
+            App.copyGivenSource(destPath, destFile, is);
+
+        } catch (final SecurityException e) {
+            if (!sourceFile.getParentFile().canRead())
+                System.out.printf(
+                        "Program JVM not currently permitted to read SOURCE's parent, `%s`.%n",
+                        sourceFile.getParentFile().getAbsolutePath());
+            else
+                System.out.printf(
+                        "Program JVM not currently permitted to read SOURCE file `%s`.%n",
+                        sourceFile.getAbsolutePath());
+            App.exitWithCode(AppExitCode.READING_NOT_ALLOWED);
+        } catch (final IOException e) {
+            System.out.printf("SOURCE file `%s` could not be fetched. Does it exist?%n", sourcePath);
+            App.exitWithCode(AppExitCode.SOURCE_NOT_FETCHED);
         }
+    }
 
-        int sourceStrId = -1, destStrId = -1;
-        boolean getPermissionForOverwrites = true;
+    private static void copyGivenSource(final String destPath, final File destFile, final FileInputStream is) {
+        try (final var os = new FileOutputStream(destFile)) {
+            int bytesRead;
+            final byte[] buffer = new byte[4096];
 
-        for (int i = 0; i < p_args.length; i++) {
-            final String s = p_args[i];
-
-            switch (s) {
-                case "-h":
-                case "--help": {
-                    App.showHelp();
-                    App.exitWithCode(AppExitCode.OK);
-                }
-                    break;
-
-                case "-f": {
-                    getPermissionForOverwrites = false;
-                }
-                    break;
-
-                default: {
-                    if (sourceStrId == -1)
-                        sourceStrId = i;
-                    else
-                        destStrId = i;
-                }
-                    break;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+                os.flush();
             }
+        } catch (final SecurityException e) {
+            if (!destFile.getParentFile().canWrite())
+                System.out.printf(
+                        "Program JVM not currently permitted to write to DEST's parent, `%s`.%n",
+                        destFile.getParentFile().getAbsolutePath());
+            else
+                System.out.printf(
+                        "Program JVM not currently permitted to write DEST file `%s`.%n",
+                        destFile.getAbsolutePath());
+            App.exitWithCode(AppExitCode.WRITING_NOT_ALLOWED);
+        } catch (final IOException e) {
+            System.out.printf("DEST file `%s` could not be created. Is there enough space?%n", destPath);
+            App.exitWithCode(AppExitCode.DESTINATION_NOT_CREATED);
         }
+    }
 
-        if (destStrId == -1) {
-            if (sourceStrId == -1) {
-                System.out.println("Neither of SOURCE or DEST was provided. Exiting.");
-                App.exitWithCode(AppExitCode.NO_PATHS_GIVEN);
-            } else {
-                System.out.println("DEST was not provided. Exiting.");
-                App.exitWithCode(AppExitCode.NO_DESTINATION_PATH);
-            }
-        }
-
-        final String destPath = p_args[destStrId];
-        final String sourcePath = p_args[sourceStrId];
-
-        File destFile = new File(destPath);
-        final File sourceFile = new File(sourcePath);
-
-        if (sourceFile.isDirectory()) {
-            System.out.println("Path `%s` provided for SOURCE points to a directory and not a file. Exiting.");
-            App.exitWithCode(AppExitCode.SOURCE_FILE_IS_DIRECTORY);
-        }
-
-        if (destFile.isDirectory())
-            destFile = new File(destFile, sourceFile.getName());
-
-        if (destFile.exists() && getPermissionForOverwrites) {
+    private static void askForOverwriting(
+            final AppPermissions permissions, final String destPath, final File destFile) {
+        if (destFile.exists() && !permissions.overwriteDestination) {
             System.out.printf("DEST file `%s` already exists. Overwrite? [Y/n]: ", destPath);
 
             final Scanner sc = new Scanner(System.in);
@@ -145,45 +147,80 @@ public class App {
                 App.exitWithoutOverwriting();
         }
 
-        try (final var is = new FileInputStream(sourceFile)) {
+        permissions.overwriteDestination = true;
+    }
 
-            try (final var os = new FileOutputStream(destFile)) {
-                int bytesRead;
-                final byte[] buffer = new byte[4096];
-
-                while ((bytesRead = is.read(buffer)) != -1) {
-                    os.write(buffer, 0, bytesRead);
-                    os.flush();
-                }
-            } catch (final SecurityException e) {
-                if (!destFile.getParentFile().canWrite())
-                    System.out.printf(
-                            "Program JVM not currently permitted to write to DEST's parent, `%s`.%n",
-                            destFile.getParentFile().getAbsolutePath());
-                else
-                    System.out.printf(
-                            "Program JVM not currently permitted to write DEST file `%s`.%n",
-                            destFile.getAbsolutePath());
-                App.exitWithCode(AppExitCode.WRITING_NOT_ALLOWED);
-            } catch (final IOException e) {
-                System.out.printf("DEST file `%s` could not be created. Is there enough space?%n", destPath);
-                App.exitWithCode(AppExitCode.DESTINATION_NOT_CREATED);
+    private static void checkParameterPresence(final AppStringIds stringIds) {
+        if (stringIds.destStrId == -1) {
+            if (stringIds.sourceStrId == -1) {
+                System.out.println("Neither of SOURCE or DEST was provided. Exiting.");
+                App.exitWithCode(AppExitCode.NO_PATHS_GIVEN);
+            } else {
+                System.out.println("DEST was not provided. Exiting.");
+                App.exitWithCode(AppExitCode.NO_DESTINATION_PATH);
             }
-
-        } catch (final SecurityException e) {
-            if (!sourceFile.getParentFile().canRead())
-                System.out.printf(
-                        "Program JVM not currently permitted to read SOURCE's parent, `%s`.%n",
-                        sourceFile.getParentFile().getAbsolutePath());
-            else
-                System.out.printf(
-                        "Program JVM not currently permitted to read SOURCE file `%s`.%n",
-                        sourceFile.getAbsolutePath());
-            App.exitWithCode(AppExitCode.READING_NOT_ALLOWED);
-        } catch (final IOException e) {
-            System.out.printf("SOURCE file `%s` could not be fetched. Does it exist?%n", sourcePath);
-            App.exitWithCode(AppExitCode.SOURCE_NOT_FETCHED);
         }
+    }
+
+    private static void parseParameters(final String[] p_args, final AppStringIds stringIds,
+            final AppPermissions permissions) {
+        for (int i = 0; i < p_args.length; i++) {
+            final String s = p_args[i];
+
+            switch (s) {
+                case "-h":
+                case "--help": {
+                    App.showHelp();
+                    App.exitWithCode(AppExitCode.OK);
+                }
+                    break;
+
+                case "-f": {
+                    permissions.overwriteDestination = false;
+                }
+                    break;
+
+                default: {
+                    if (stringIds.sourceStrId == -1)
+                        stringIds.sourceStrId = i;
+                    else
+                        stringIds.destStrId = i;
+                }
+                    break;
+            }
+        }
+    }
+
+    public static void main(final String[] p_args) {
+        if (p_args.length < 2) {
+            App.showHelp();
+            App.exitWithCode(AppExitCode.OK);
+        }
+
+        final AppStringIds stringIds = new AppStringIds();
+        final AppPermissions permissions = new AppPermissions();
+
+        App.parseParameters(p_args, stringIds, permissions);
+        App.checkParameterPresence(stringIds);
+
+        final String destPath = p_args[stringIds.destStrId];
+        final String sourcePath = p_args[stringIds.sourceStrId];
+
+        File destFile = new File(destPath);
+        final File sourceFile = new File(sourcePath);
+
+        if (sourceFile.isDirectory()) {
+            System.out.printf(
+                    "Path `%s` provided for SOURCE points to a directory and not a file. Exiting.%n",
+                    sourcePath);
+            App.exitWithCode(AppExitCode.SOURCE_FILE_IS_DIRECTORY);
+        }
+
+        if (destFile.isDirectory())
+            destFile = new File(destFile, sourceFile.getName());
+
+        App.askForOverwriting(permissions, destPath, destFile);
+        App.performActualCopy(destPath, sourcePath, destFile, sourceFile);
     }
 
 }
