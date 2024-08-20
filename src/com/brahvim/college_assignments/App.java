56bf84f6eb9e9ffa9f44cd9de1b24fc8412567a4
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-20 21:14:58 +0530

    Eh, what else? I want to move on too, haha.

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -2,6 +2,7 @@ package com.brahvim.college_assignments;
 
 import java.io.IOException;
 
+import com.brahvim.college_assignments.math.vectors.Vector3;
 import com.brahvim.college_assignments.model.loading.ObjModelLoader;
 import com.brahvim.college_assignments.vehicles.Car;
 
@@ -10,11 +11,17 @@ public class App {
 	public static void main(final String[] p_args) {
 		final Car car = new Car();
 
+		// Load the model if you can:
 		try (final ObjModelLoader loader = new ObjModelLoader("./res/models/car-model.obj")) {
 			car.setModel(loader.load());
 		} catch (final IOException e) {
 			e.printStackTrace();
 		}
+
+		// Move it:
+		car.getTransform()
+				.getTranslation()
+				.add(new Vector3(100, 500, 60));
 	}
 
 }
