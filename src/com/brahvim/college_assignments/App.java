f7983bac5878931c18d50fdc0d686829936492ff
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-21 08:59:49 +0530

    Good morning. I'm committing this now instead.

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -2,8 +2,12 @@ package com.brahvim.college_assignments;
 
 public class App {
 
-    public static void main(final String[] p_args) {
+	public static void main(final String[] p_args) {
+		// Learn to use Maven/Gradle/Ant (okay, NOT ANT!) or simply add in this JAR when
+		// compiling - build systems will allow you to update as needed: [
+		// https://dlm.mariadb.com/3852266/Connectors/java/connector-java-3.4.1/mariadb-java-client-3.4.1.jar
+		// ].
 
-    }
+	}
 
 }
