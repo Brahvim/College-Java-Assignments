30b98c82e679521adc77f29cb98688547ad3e17a
Brahvim <69293652+Brahvim@users.noreply.github.com>
2024-08-29 12:00:10 +0530

    Playing around...

--- src/com/brahvim/college_assignments/App.java
+++ src/com/brahvim/college_assignments/App.java
@@ -1,9 +1,27 @@
 package com.brahvim.college_assignments;
 
-public class App {
+import java.applet.Applet;
 
-    public static void main(final String[] p_args) {
+@Deprecated
+public class App extends Applet {
 
-    }
+	public static void main(final String[] p_args) {
+		final App app = new App();
+		app.start();
+	}
+
+	@Override
+	public void start() {
+		System.out.println("Started!");
+		System.out.printf("Applet info string: `%s`%n", super.getAppletInfo());
+
+		// *STRAIGHT* to an NPE!:
+		// System.out.printf("Applet context: `%s`%n", super.getAppletContext());
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("Applet destroyed...");
+	}
 
 }
